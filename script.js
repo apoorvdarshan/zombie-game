e66c8c5f1021019e66c08b077cb2a2f5d68ee242
Apoorv Darshan <ad13dtu@gmail.com>
2026-09-07 09:07:59 +0530

    Fix Instagram handle to @apoorvcodes

--- script.js
+++ script.js
@@ -641,7 +641,7 @@ function createBurnedBuildings() {
 
       ctx.font = "bold 32px Arial";
       ctx.fillText("YouTube: @apoorvdarshan", canvas.width / 2, 170);
-      ctx.fillText("Instagram: @apoorvcodex", canvas.width / 2, 220);
+      ctx.fillText("Instagram: @apoorvcodes", canvas.width / 2, 220);
 
       ctx.font = "bold 28px Arial";
       ctx.fillText("LIKE & SUBSCRIBE!", canvas.width / 2, 290);
